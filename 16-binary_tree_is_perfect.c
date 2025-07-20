728c78cd84a7280bd3277cc9387d8526a979c005
macximiliam <alexanderbigman29@gmail.com>
2025-07-20 05:21:14 +0000

    Add function that checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -42,7 +42,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 		if (hl == hr)
 			return (binary_tree_is_perfect(tree->left) &&
-			        binary_tree_is_perfect(tree->right));
+				binary_tree_is_perfect(tree->right));
 	}
 
 	return (0);
