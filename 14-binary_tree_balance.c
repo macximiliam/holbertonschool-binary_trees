ebd6d795d19abf0f26f27d40d081b38f77288027
macximiliam <alexanderbigman29@gmail.com>
2025-07-20 01:28:46 +0000

    Add a function about of binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -31,6 +31,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	size_t height_left = 0, height_right = 0;
 
+	if ((tree == NULL) || ((tree->left == NULL) &&  (tree->right == NULL)))
+		return (0);
 	if (tree->left)
 		height_left = binary_tree_height(tree->left) + 1;
 	if (tree->right)
